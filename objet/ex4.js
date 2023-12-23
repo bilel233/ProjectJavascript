418a6a23dce7dd83cea82f04def21b0d96c5ba22
bilel233 <bilel.mjdb@gmail.com>
2023-12-23 10:49:34 +0100

    aide memoire sur les objets mathematiques

---
+++ objet/ex4.js
@@ -0,0 +1,24 @@
+/* aide memoire sur les objets mathematiques */
+
+console.log(Math.PI);
+console.log(Math.E);
+console.log(Math.LN2);
+
+// methodes arrondi
+
+console.log(Math.ceil(10.2));
+console.log(Math.ceil(10.2));
+console.log(Math.floor(10.2));
+console.log(Math.trunc(10.1234));
+console.log(Math.trunc(10.2));
+console.log(Math.round(11.2));
+console.log(Math.round(11.7));
+
+// methodes arithmetiques et de calcul
+
+console.log(Math.pow(2, 3));
+console.log(Math.sqrt(16));
+console.log(Math.cbrt(8));
+console.log(Math.abs(-10));
+console.log(Math.min(1, 2, 3, 0, 10));
+console.log(Math.max(1, 2, 3, 0, 10));
