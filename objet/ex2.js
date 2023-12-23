03c24ef7fdc73ff7b86a17a02d892574df366b87
bilel233 <bilel.mjdb@gmail.com>
2023-12-23 10:14:10 +0100

    les litteraux d'objets

---
+++ objet/ex2.js
@@ -0,0 +1,22 @@
+/* les litteraux d'objets et la notation entre crochets */
+
+let house2 = {};
+house2["rooms"] = 4;
+house2["color"] = "pink";
+house2["priceUSD"] = 12345;
+console.log(house2);
+
+// avec une voiture
+
+let car = {};
+
+car.color = "red";
+car["color"] = "greeen";
+car["speed"] = 200;
+car.speed = 100;
+console.log(car);
+
+// caracteres d'espacements
+
+car["number of doors"] = 4;
+console.log(car);
