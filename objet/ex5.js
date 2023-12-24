37c5c0978c4236b937099e511737c5edcb063497
bilel233 <bilel.mjdb@gmail.com>
2023-12-24 14:49:24 +0100

    aide memoire sur les cordes

---
+++ objet/ex5.js
@@ -0,0 +1,13 @@
+/* aide memoire sur les cordes
+
+*/
+
+let greet = "hello";
+let place = "world";
+
+greet.length;
+console.log(greet.length);
+
+// lire chaque caractre de maniere individuel
+
+greet.charAt(0);
