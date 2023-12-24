3f912d3740553f0a6ba1f212fab474bb9e1bae53
bilel233 <bilel.mjdb@gmail.com>
2023-12-24 15:05:45 +0100

    Imprimer tous les nombres pairs de 1 à 100

---
+++ boucles/ex3.js
@@ -0,0 +1,9 @@
+/* imprimez tous les nombres pairs de 1 a 100 */
+
+// on part avec une boucle for
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 == 0) {
+    console.log(i); // i est pair
+  }
+}
