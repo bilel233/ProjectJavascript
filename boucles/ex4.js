2b8007ca4c3bedc1339a6a7e867e6f39089e05fe
bilel233 <bilel.mjdb@gmail.com>
2023-12-24 15:09:42 +0100

    imprimez tous les nombres divisibles par 5 de 1 a 100

---
+++ boucles/ex4.js
@@ -0,0 +1,7 @@
+/* imprimez tous les nombres divisibles par 5 de 1 a 100 */
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 5 == 0) {
+    console.log(i); // i est divisible par 5 ; donc on l'affiche
+  }
+}
