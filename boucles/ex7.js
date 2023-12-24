6c5760c0048f12b62b466e9ad5d0e7378b6f257c
bilel233 <bilel.mjdb@gmail.com>
2023-12-24 15:44:13 +0100

    Calculer la somme de tous les nombres de 1 à 100

---
+++ boucles/ex7.js
@@ -0,0 +1,7 @@
+/* calcul de la somme de tous les nombres de 1 à 100 */
+let s = 0;
+
+for (let i = 1; i <= 100; i++) {
+  s = s + i; // calcul de la somme
+}
+console.log(s); // affichage de la somme
