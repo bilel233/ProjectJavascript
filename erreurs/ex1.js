1abb975f653eb29d8df34e8a66a7cfb8f93cfbfb
bilel233 <bilel.mjdb@gmail.com>
2023-12-25 12:31:43 +0100

    erreurs de syntaxe , de logique et d'execution

---
+++ erreurs/ex1.js
@@ -0,0 +1,13 @@
+/* erreurs de syntaxe , de logique et d'execution */
+
+// ereurs de reference
+
+console.log(username);
+
+// SyntaxError
+
+let a "there's no assignment operator here";
+
+// ereur de type
+
+"hello".pop();
